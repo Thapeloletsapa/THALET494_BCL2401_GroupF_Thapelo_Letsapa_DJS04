61cbb7821aa1472d119ea1bb608d1c02cb1b1fdb
Thapelo <thapeloletsapa@gmail.com>
2024-05-17 14:49:28 +0200

    create book-review.js

---
+++ book-preview.js
@@ -0,0 +1,26 @@
+import { authors } from './data.js';
+
+class BookPreview extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+    <link rel="stylesheet" href="book-preview-styles.css">
+      
+        <div class="overlay">
+          <div class="overlay__preview">
+            <img class="overlay__blur" data-list-blur src="" />
+            <img class="overlay__image" data-list-image src="" />
+          </div>
+          <div class="overlay__content">
+            <h3 class="overlay__title" data-list-title></h3>
+            <div class="overlay__data" data-list-subtitle></div>
+            <p class="overlay__data overlay__data_secondary" data-list-description></p>
+          </div>
+          <div class="overlay__row">
+            <button class="overlay__button overlay__button_primary" data-list-close>Close</button>
+          </div>
+        </div>
+      
+      `;
+  }
